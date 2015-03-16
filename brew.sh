5d0683ef5763825fef8e763746ee4356b1751fb2
Philip Gardner <philip@no-logo.co.uk>
2015-03-16 16:53:53 +0000

    bump

--- brew.sh
+++
@@ -1,3 +0,0 @@
-for app in {ack,boost,cmake,freetype,gettext,gist,git,git-extras,glew,glm,gnu-typist,gource,hub,jpeg,lftp,libevent,libmemcached,libpng,libtiff,memcached,nmap,node,opencv,ossp-uuid,pcre,pkg-config,postgresql,readline,scala,sdl,sdl_image,solr,watch,webp,wget};
-do brew install $app;
-done;
